da5de4207b20ad0d2f8f2ffaa5231d8789a6a718
Nolawi Teklehaimanot <nolawiteklehaimanot@gmail.com>
2023-04-25 10:55:51 -0400

    documented the javascript code so far

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-let titles = document.getElementById('titles')
-let table = document.getElementById('mainTable')
+let titles = document.getElementById('titles') // get titles from HTML
+let table = document.getElementById('mainTable') // get table from HTML
 
-number = -1
-for (var i=0;i<=18;i++){
-    number = parseInt(number) + 1
-    var header = document.createElement('th')
-    if (i == 0){
+let number_of_courses = 8 // get the number of courses, will be from api soon
+
+number = -1 // set a number at -1
+for (var i=0;i<=18;i++){ // run loop 18 times for each unit header
+    number = parseInt(number) + 1 // convert back to number and add one for each unit header
+    var header = document.createElement('th') // create a header
+    if (i == 0){ // if it is the first iteration of the loop, make the header say 'Course'
         header.innerText = 'Course'
-    } else {
-        console.log(number.toString().length)
-        if (number.toString().length == 1){
-            console.log(number)
+    } else { // else, the header is the unit number
+        if (number.toString().length == 1){ // convert single digit numbers to double digit numbers (1-->01)
             number = ("0" + number).slice(-2)
         }
-        header.innerText = number
+        header.innerText = number // make the header the number
     }
-    titles.appendChild(header)
+    titles.appendChild(header) // append the header to the title
 }
 
-for (var i=0; i<8; i++){
-    var row = document.createElement('tr')
-    row.align = 'center'
-    for (var j=0; j<=18; j++){
-        var box = document.createElement('td')
+for (var i=0; i<number_of_courses; i++){ // run a loop for each course the user has
+    var row = document.createElement('tr') // create a row for each course
+    row.align = 'center' // align the row to the center
+    for (var j=0; j<=18; j++){ // for each course, run a loop 18 times for 18 units
+        var box = document.createElement('td') // create a box for each unit
         if (j == 0){
-            box.innerText = 'MCV4U1-02'
+            box.innerText = 'MCV4U1-02' // if the box is the first in each row, make it the course code + section
         }
-        row.appendChild(box)
+        row.appendChild(box) // append the box to the row
     }
-    table.appendChild(row)
+    table.appendChild(row) // append the row to the table
 }
