d065da7f2c31befdfdb5999c0b7cc46fe02f21a8
Nolawi Teklehaimanot <nolawiteklehaimanot@gmail.com>
2023-04-28 14:31:26 -0400

    added 6 digit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+
+const nums = document.querySelectorAll('.num')
+const form = document.querySelector('form')
+
+nums.forEach((num, index) => {
+    num.dataset.id = index
+    
+    num.addEventListener('keyup', function(event) {
+        if (event.keyCode === 13) form.submit()
+        if (num.value.length == 1) {
+            nums[parseInt(num.dataset.id) + 1].focus()
+        }
+        if (event.keyCode === 8) {
+            nums[parseInt(num.dataset.id) - 1].focus()
+        }
+    })
+})
