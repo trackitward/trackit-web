23af34183f51b21b1845a55eef5034f743f1363b
5late <christianrfernandes5@gmail.com>
2023-05-28 23:33:50 -0400

    fix the mess and optimize the unit tracker

---
+++ js/main.js
@@ -0,0 +1,82 @@
+let titles = document.getElementById('titles') // get titles from HTML
+let table = document.getElementById('mainTable') // get table from HTML
+
+let mainURL = 'http://localhost:31475'
+let current_course = 0;
+let number_of_courses = 5;
+
+var user_data = {}
+let fetched = false
+
+window.onload = getAPI()
+
+function getAPI() {
+    student_num = "138213763"
+    if (fetched){return}
+    fetch(mainURL+"/get/user/"+student_num+"/all_data").then(response => response.json()).then(data => {
+        console.log("this should be first", data)
+        user_data = data
+        if (data.data.course_data.length > 0){
+            createPage()
+        }
+    })
+    fetched = true
+}
+
+function fillCourseBox(box){
+    getAPI()
+    student_num = "138213763"
+    if (current_course <= user_data.data.course_data.length) {
+        //console.log(user_data.data.course_data[current_course])
+        box.innerHTML = user_data.data.course_data[current_course].user_course.course_info.course_code
+        current_course += 1
+    }
+    console.log(fetched)
+}
+
+function fillUnitBox(index, box, current_course){
+    index -= 1
+    current_course -= 1
+    getAPI()
+    student_num = "138213763"
+    //console.log(current_course, index, user_data.data.course_data[current_course].user_course.user_info.units[index].unit_completed)
+    if (user_data.data.course_data[current_course].user_course.user_info.units[index].unit_completed) {
+        box.innerHTML = "✔️"
+    } else {
+        box.innerHTML = "❌"
+    }
+}
+
+//let number_of_courses = 8 // get the number of courses, will be from api soon
+
+function createPage(){
+    number = -1 // set a number at -1
+    for (var i=0;i<=18;i++){ // run loop 18 times for each unit header
+        number = parseInt(number) + 1 // convert back to number and add one for each unit header
+        var header = document.createElement('th') // create a header
+        if (i == 0){ // if it is the first iteration of the loop, make the header say 'Course'
+            header.innerText = 'Course'
+        } else { // else, the header is the unit number
+            if (number.toString().length == 1){ // convert single digit numbers to double digit numbers (1-->01)
+                number = ("0" + number).slice(-2)
+            }
+            header.innerText = number // make the header the number
+        }
+        titles.appendChild(header) // append the header to the title
+    }
+
+    for (var i=0; i<=number_of_courses; i++){ // run a loop for each course the user has
+        var row = document.createElement('tr') // create a row for each course
+        row.align = 'center' // align the row to the center
+        for (var j=0; j<=18; j++){ // for each course, run a loop 18 times for 18 units
+            var box = document.createElement('td') // create a box for each unit
+            if (j == 0){
+                fillCourseBox(box)
+            } else {
+                fillUnitBox(j, box, current_course)
+            }
+            row.appendChild(box) // append the box to the row
+        }
+        table.appendChild(row) // append the row to the table
+    }
+}
