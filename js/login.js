2abe474ad2cd1037020c3d583f228c4ba081122b
5late <christianrfernandes5@gmail.com>
2023-06-09 22:42:54 -0400

    connect backend

--- js/login.js
+++ js/login.js
@@ -1,18 +1,34 @@
+let api_link = 'http://localhost:31475'
+const popup = document.querySelector(".popup");
+
 async function attepmtLogin(){
-    let student_num_elem = document.getElementById('student-elem')
+    let student_num_elem = document.getElementById('student-num')
     let password_elem = document.getElementById('password')
 
     let data = {
-        'student_number': student_num_elem.innerText,
-        'password': password_elem.innerText
+        'student_number': student_num_elem.value,
+        'password': password_elem.value
     }
 
     const response = await fetch(`${api_link}/post/user/profile/auth`, {
         method: "POST",
-        body: JSON.stringify(data.toString())
+        body: JSON.stringify(data)
     })
 
     if (response.status == 202) {
         location.href = '/index.html'
+    } else {
+        popup.style.display = "flex"
+        popup.style.left = "2vw";
+        setTimeout(() => {
+          popup.style.left = "-100vw";
+        }, 3000);
     }
 }
+
+let submit = document.getElementById('submit')
+
+submit.addEventListener('click', (e) => {
+    attepmtLogin()
+    e.preventDefault()
+})
